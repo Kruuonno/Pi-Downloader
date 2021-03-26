1bfdf0d166d1658f036ac74f4a1486f85080e6f2
Kruuonno <81293114+Kruuonno@users.noreply.github.com>
2021-03-26 01:29:41 -0700

    Update usb-mount.sh

--- scripts/usb-mount.sh
+++ scripts/usb-mount.sh
@@ -2,7 +2,6 @@
 #sudo -i
 sudo mkdir /home/pi/data
 grep -q 'data' /etc/fstab || printf '# data\n/dev/sdb1    /home/pi/data    ext4    defaults    0    2\n' >> /etc/fstab
-exit
-sudo mount -a
 sudo chmod 1777 /home/pi/data
+sudo mount -a
 
