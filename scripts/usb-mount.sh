05ac0943d7d959efeb8587270a34980dc2add967
Kruuonno <81293114+Kruuonno@users.noreply.github.com>
2021-03-27 02:36:02 -0700

    Update usb-mount.sh

--- scripts/usb-mount.sh
+++ scripts/usb-mount.sh
@@ -1,7 +1,8 @@
 #sudo umount /dev/sdb1
 #sudo -i
 mkdir /home/pi/data
-sudo grep -q 'data' /etc/fstab || printf '# data\n//dev/sdb1 /home/pi/data ntfs-3g uid=pi,gid=pi 0 0\n' >> /etc/fstab
+echo "/dev/sdb1    /home/pi/data    ntfs-3g     uid=pi,gid=pi 0 0" | sudo tee -a /etc/fstab
+#sudo grep -q 'data' /etc/fstab || printf '# data\n//dev/sdb1 /home/pi/data ntfs-3g uid=pi,gid=pi 0 0\n' >> /etc/fstab
 sudo chmod 1777 /home/pi/data
 sudo mount -a
 
