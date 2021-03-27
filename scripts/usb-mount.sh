3e1edacbb7806cfb1e9370d6594f3630e5043d37
Kruuonno <81293114+Kruuonno@users.noreply.github.com>
2021-03-27 01:37:32 -0700

    Update usb-mount.sh

--- scripts/usb-mount.sh
+++ scripts/usb-mount.sh
@@ -1,7 +1,7 @@
 #sudo umount /dev/sdb1
 #sudo -i
 sudo mkdir /home/pi/data
-grep -q 'data' /etc/fstab || printf '# data\n/dev/sdb1    /home/pi/data    ext4    defaults    0    2\n' >> /etc/fstab
+grep -q 'data' /etc/fstab || printf '# data\n//dev/sdb1	/home/pi/data	ntfs-3g	uid=pi,gid=pi	0	0\n' >> /etc/fstab
 sudo chmod 1777 /home/pi/data
 sudo mount -a
 
